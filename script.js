bc902a90545e1ccc6756e70b08bdcc32af515f1f
shashank-chepuri <138128922+shashank-chepuri@users.noreply.github.com>
2024-06-12 23:44:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ var menueIcon = document.querySelector(".menu-icon");
+ var sidebar = document.querySelector(".sidebar");
+ var container = document.querySelector(".container");
+
+ menueIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("big-container");
+ } 
